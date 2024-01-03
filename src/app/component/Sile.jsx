9855296e09846d54cee24c25c68194062f92b031
MD Shaharia <adeptshaharia@gmail.com>
2024-01-03 16:16:07 +0600

    responsive_done

--- src/app/component/Sile.jsx
+++ src/app/component/Sile.jsx
@@ -1,5 +1,5 @@
 'use client'
-// import '../app/globals.css'
+
 import { AiFillSafetyCertificate } from "react-icons/ai";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { TiHome } from "react-icons/ti";
@@ -8,29 +8,25 @@ import { RiProfileFill } from "react-icons/ri";
 import { FaHistory } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { AiFillCaretUp } from "react-icons/ai";
-import { FaCheckSquare } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
-import { AiOutlineDown } from "react-icons/ai";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Res } from "./Redux/slice";
-
 const Sideber = () => {
   const dispatch = useDispatch()
-  const [hide, setHide] = useState(true);
   const responsive = useSelector((state) =>state.shaharia.value);
 
 
     return (
-        <div className={`  lg:h-screen lg:sticky top-0 left-0 z-50   dark:text-white  h-auto shadow-md`}>
+        <div className={`  lg:h-screen lg:sticky fixed top-0 left-0 z-50   dark:text-white  h-auto shadow-md`}
+        onMouseEnter={()=>dispatch(Res())}
+        onMouseLeave={()=>dispatch(Res())} >
         <div
-          className={`h-screen sidebar bg-[#121D45] shadow-sm shadow-right  ${responsive?'block':'hidden'} md:block   relative  text-white  ${
-            hide ? " w-64 md:w-72  " : "w-20  "
+          className={`h-screen   bg-[#121D45] shadow-sm shadow-right  ${responsive?'block':'hidden'} md:block   relative  text-white  ${
+            responsive? " w-64 2xl:w-72 lg:w-64  " : "w-20  "
           }`}
-        >
-          {hide ? (
-            <div className="flex px-10  items-center relative pt-10">
+         >
+          {responsive? (
+            <div className="flex px-10  items-center relative pt-20">
               <AiFillSafetyCertificate className="text-secondary  text-4xl font-medium " />
               <p className="text-[24px] font-bold lg:text-3xl text-shadow">
                 {" "}
@@ -38,7 +34,8 @@ const Sideber = () => {
               </p>
               <AiOutlineArrowLeft
                 className="bg-white md:block hidden text-secondary   w-8 h-8 cursor-pointer shadow-lg rounded-full absolute right-[-10px]"
-                onClick={() => setHide((prev) => !prev)}
+                // onClick={() => setHide((prev) => !prev)}
+                onClick={()=>dispatch(Res())}
               />
               <AiOutlineArrowLeft
                 className="bg-white block md:hidden text-secondary  w-8 h-8 cursor-pointer shadow-lg rounded-full absolute right-[-10px]"
@@ -47,72 +44,73 @@ const Sideber = () => {
             </div>
           ) : (
             <li
-              className={`flex  items-center ${
-                !hide && "justify-center"
+              className={`flex  items-center pt-20 ${
+                !responsive&& "justify-center"
               } gap-4 py-4 pt-10`}
             >
               <GiHamburgerMenu
                 className="text-secondary   text-3xl font-medium cursor-pointer "
-                onClick={() => setHide((prev) => !prev)}
+                // onClick={() => setHide((prev) => !prev)}
+                onClick={()=>dispatch(Res())}
               />
             </li>
           )}
 
-          <div className={` ${hide ? "px-10 mt-16" : "px-0 mt-16"}`}>
+          <div className={` ${responsive? "px-10 mt-16" : "px-0 mt-16"}`}>
             <ul>
               <li
                 className={`flex  items-center ${
-                  !hide && "justify-center"
+                  !responsive&& "justify-center"
                 } gap-4 py-4`}
               >
                 <TiHome className=" text-3xl font-medium " />
-                {hide && <p className="text-16px  ">Home</p>}
+                {responsive&& <p className="text-16px  ">Home</p>}
               </li>
               <li
                 className={`flex  items-center ${
-                  !hide && "justify-center"
+                  !responsive&& "justify-center"
                 } gap-4 py-4`}
               >
                 <CgProfile className=" text-3xl font-medium " />
-                {hide && <p className="text-16px">Patient Profile </p>}
+                {responsive&& <p className="text-16px">Patient Profile </p>}
               </li>
 
               <li
                 className={`flex  items-center ${
-                  !hide && "justify-center"
+                  !responsive&& "justify-center"
                 } gap-4 py-4`}
               >
                 <RiProfileFill className=" text-3xl font-medium " />
-                {hide && <p className="text-16px ">Appointments</p>}
+                {responsive&& <p className="text-16px ">Appointments</p>}
               </li>
               <li
                 className={`flex  items-center ${
-                  !hide && "justify-center"
+                  !responsive&& "justify-center"
                 } gap-4 py-4`}
               >
                 <FaHistory className=" text-3xl font-medium " />
 
-                {hide && <p className="text-16px">Medical History</p>}
+                {responsive&& <p className="text-16px">Medical History</p>}
               </li>
               <li
                 className={`flex  items-center ${
-                  !hide && "justify-center"
+                  !responsive&& "justify-center"
                 } gap-4 py-4`}
               >
                 <IoSettingsSharp className=" text-3xl font-medium " />
-                {hide && <p className="text-16px ">Settings</p>}
+                {responsive&& <p className="text-16px ">Settings</p>}
               </li>
             </ul>
           </div>
           <div className="absolute bottom-3  w-full flex justify-center">
-            {hide ? (
+            {responsive? (
               <p className="text-16px font-semibold   badge-secondary  w-auto  rounded-md  px-10 py-2 text-white ">
                 Contact US
               </p>
             ) : (
               <li
                 className={`flex  items-center ${
-                  !hide && "justify-center"
+                  !responsive&& "justify-center"
                 } gap-4 py-4`}
               >
                 <div className="h-12 w-12 badge-secondary  flex items-center justify-center rounded-3xl">
--- src/app/page.js
+++ src/app/page.js
@@ -16,65 +16,102 @@ export default function Home() {
   
   return (
  <div className=" w-full">
-     <div className="md:pl-28 lg:mt-28 dark:bg-[#150A09]  w-full lg:pl-10 pl-0 md:pr-7 md:pt-0 md:py-10  ">
+     <div className=" md:pl-[85px] md:pr-[10px] 2xl:mt-24 lg:mt-20 mt-16 md:mt-16 dark:bg-[#150A09]  w-full lg:px-7 pl-0  md:pt-0 md:py-10  ">
         {/* Main Div */}
         <div className={` lg:gap-x-8 lg:grid lg:grid-cols-12 '}`} >
+         
           <div className="lg:col-span-8 ">
             {/* main Breaking News ********************************************************/}
             <article className="bg-gradient-to-r   text-white relative ">
                  <div className="m-0 p-0 w-full braking_img relative after:bg-slate-800">
-                  <img className=" h-[500px] w-[100%]"  alt="doctor" src={Banner}/>
+                  <img className=" 2xl:h-[500px] lg:h-[340px] w-[100%]"  alt="doctor" src={Banner}/>
                   </div>
                  <div className="flex px-5 pb-3 absolute bottom-0 left-0  flex-col w-full ">
-                <a href='#' className="text-5xl text-shadow-md font-bold hover:text-gray-200">Powerful quake in Japan tears down homes and triggers tsunami warnings</a>
+                <a href='#' className="2xl:text-5xl lg:text-3xl md:text-2xl text-shadow-md font-bold hover:text-gray-200">Powerful quake in Japan tears down homes and triggers tsunami warnings</a>
                 <p className="mt-3">magnitude earthquake in Japan has caused homes to collapse, ripped up roads and triggered tsunami warnings for coastal areas</p> 
                  </div>
             </article>
 
             {/* Sub Breaking News ********************************************************/}
-            <div className="grid md:grid-cols-3 grid-cols-2 py-10 gap-4">
-            <article className="bg-gradient-to-r   text-white relative ">
-                 <div className="m-0 p-0 w-full braking_img relative after:bg-slate-800">
-                  <img className=" h-[250px] w-[100%]"  alt="doctor" src={Prime}/>
+            <div className="grid md:grid-cols-3 grid-cols-2 2xl:py-10 lg:py-3 lg:gap-4 gap-2 mt-2 lg:mt-0">
+            <article className="bg-gradient-to-r   text-white relative 2xl:h-[250px] lg:h-[200px]">
+                 <div className="m-0 p-0 w-full braking_img relative after:bg-slate-800 h-full">
+                  <img className="h-[100%] w-[100%] "  alt="doctor" src={Prime}/>
                   </div>
                  <div className="flex px-5 pb-3 absolute bottom-0 left-0  flex-col w-full ">
-                <a href='#' className="text-[18px] text-shadow-md font-bold hover:text-gray-200">Powerful quake in Japan tears down homes and triggers tsunami warnings</a>
+                <a href='#' className="2xl:text-[18px] text-[12px]  text-shadow-md font-bold hover:text-gray-200">Powerful quake in Japan tears down homes and triggers tsunami warnings</a>
                  </div>
             </article>
-            <article className="bg-gradient-to-r   text-white relative ">
-                 <div className="m-0 p-0 w-full braking_img relative after:bg-slate-800">
-                  <img className=" h-[250px] w-[100%]"  alt="doctor" src={japan}/>
+            <article className="bg-gradient-to-r   text-white relative 2xl:h-[250px] lg:h-[200px]">
+                 <div className="m-0 p-0 w-full braking_img relative after:bg-slate-800 h-full">
+                  <img className=" h-[100%] w-[100%]"  alt="doctor" src={japan}/>
                   </div>
                  <div className="flex px-5 pb-3 absolute bottom-0 left-0  flex-col w-full ">
-                <a href='#' className="text-[18px] text-shadow-md font-bold hover:text-gray-200">Powerful quake in Japan tears down homes and triggers tsunami warnings</a>
+                <a href='#' className="text-[12px] 2xl:text-[18px] text-shadow-md font-bold hover:text-gray-200">Powerful quake in Japan tears down homes and triggers tsunami warnings</a>
                  </div>
             </article>
-            <article className="bg-gradient-to-r   text-white relative ">
-                 <div className="m-0 p-0 w-full braking_img relative after:bg-slate-800">
-                  <img className=" h-[250px] w-[100%]"  alt="doctor" src={Uck}/>
+            <article className="bg-gradient-to-r   text-white relative 2xl:h-[250px] lg:h-[200px]">
+                 <div className="m-0 p-0 w-full braking_img relative after:bg-slate-800 h-full">
+                  <img className=" h-[100%] w-[100%]"  alt="doctor" src={Uck}/>
                   </div>
                  <div className="flex px-5 pb-3 absolute bottom-0 left-0  flex-col w-full ">
-                <a href='#' className="text-[18px] text-shadow-md font-bold hover:text-gray-200">Powerful quake in Japan tears down homes and triggers tsunami warnings</a>
+                <a href='#' className="text-[12px] 2xl:text-[18px]  text-shadow-md font-bold hover:text-gray-200">Powerful quake in Japan tears down homes and triggers tsunami warnings</a>
                  </div>
             </article>
             </div>
           </div>
 
           {/* Side bar News ************************************************************/}
-           <div className="lg:col-span-4 ">
-            <article className="grid grid-cols-2 gap-5">
-              
-              <div className="car bg-base-100 shadow-sm">
-                <figure><img src={Trump} alt="img" className='h-44' /></figure>
+           <div className="lg:col-span-4 mt-2 lg:mt-0 ">
+            <article className="grid grid-cols-2 grid-rows-2 2xl:gap-5 gap-2 ">
+          
+              <div className="car bg-base-100 shadow-sm  2xl:h-96 ">
+                <figure><img src={Trump} alt="img" className='2xl:h-44 lg:h-32' /></figure>
                 <div className="py-2 px-1">
-                  <div> <a href='#' className=" font-bold">
+                  <div className='lg:leading-3 2xl:leading-5'> <a href='#' className=" font-bold lg:text-[12px] 2xl:text-[16px] ">
                   Unprecedented Colorado ruling puts courts at the center of Trump’s fate next year
                   </a></div>
                   <div className="badge badge-secondary">NEW</div>
-                  <p className='mt-3'>Russian officials say the border city of Belgorod, where 25 people were killed on Saturday, </p>
+                  <p className='2xl:mt-3 mt-1 lg:leading-3 2xl:leading-4 lg:text-[12px] l2xl:text-[14px]'>Russian officials say the border city of Belgorod, where 25 people were killed on Saturday,
+                 </p>
                  </div>
                 </div>
-              <div className="car bg-base-100 shadow-sm">
+              <div className="car bg-base-100 shadow-sm 2xl:h-96 ">
+                <figure><img src={Byden} alt="img" className='2xl:h-44 lg:h-32' /></figure>
+                <div className="py-2 px-1">
+                  <div className='lg:leading-3 2xl:leading-5'> <a href='#' className=" font-bold lg:text-[12px] 2xl:text-[16px] ">
+                  Unprecedented Colorado ruling puts courts at the center of Trump’s fate next year
+                  </a></div>
+                  <div className="badge badge-secondary">NEW</div>
+                  <p className='2xl:mt-3 mt-1 lg:leading-3 2xl:leading-4 lg:text-[12px] l2xl:text-[14px]'>Russian officials say the border city of Belgorod, where 25 people were killed on Saturday,
+                 </p>
+                 </div>
+                </div>
+              <div className="car bg-base-100 shadow-sm 2xl:h-96">
+                <figure><img src={Putin} alt="img" className='2xl:h-44 lg:h-32' /></figure>
+                <div className="py-2 px-1">
+                  <div className='lg:leading-3 2xl:leading-5'> <a href='#' className=" font-bold lg:text-[12px] 2xl:text-[16px] ">
+                  Unprecedented Colorado ruling puts courts at the center of Trump’s fate next year
+                  </a></div>
+                  <div className="badge badge-secondary">NEW</div>
+                  <p className='2xl:mt-3 mt-1 lg:leading-3 2xl:leading-4 lg:text-[12px] l2xl:text-[14px]'>Russian officials say the border city of Belgorod, where 25 people were killed on Saturday,
+                 </p>
+                 </div>
+                </div>
+              <div className="car bg-base-100 shadow-sm 2xl:h-96 ">
+                <figure><img src={jokar} alt="img" className='2xl:h-44 lg:h-32' /></figure>
+                <div className="py-2 px-1">
+                  <div className='lg:leading-3 2xl:leading-5'> <a href='#' className=" font-bold lg:text-[12px] 2xl:text-[16px] ">
+                  Unprecedented Colorado ruling puts courts at the center of Trump’s fate next year
+                  </a></div>
+                  <div className="badge badge-secondary">NEW</div>
+                  <p className='2xl:mt-3 mt-1 lg:leading-3 2xl:leading-4 lg:text-[12px] l2xl:text-[14px]'>Russian officials say the border city of Belgorod, where 25 people were killed on Saturday,
+                 </p>
+                 </div>
+                </div>
+            
+           
+              {/* <div className="car bg-base-100 shadow-sm">
                 <figure><img src={jokar} alt="img" className='h-44' /></figure>
                 <div className="py-2 px-1">
                   <div> <a href='#' className=" font-bold">
@@ -104,7 +141,7 @@ export default function Home() {
                   <p className='mt-3'>Russian officials say the border city of Belgorod, where 25 people were killed on Saturday, </p>
                  </div>
                 </div>
-           
+            */}
             </article>
             {/* <article className="px-5 bg-white dark:bg-[#2c2121] mt-3">
               <div className="flex justify-between py-4 px-2"><p>Covid-19 Updates</p>
