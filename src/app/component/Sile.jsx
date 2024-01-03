1d9f93781cb54dab04d04fa57603b1353ae934a2
MD Shaharia <adeptshaharia@gmail.com>
2024-01-03 13:25:31 +0600

    Breaking_news_done

--- src/app/component/Sile.jsx
+++ src/app/component/Sile.jsx
@@ -25,23 +25,23 @@ const Sideber = () => {
     return (
         <div className={`  lg:h-screen lg:sticky top-0 left-0 z-50   dark:text-white  h-auto shadow-md`}>
         <div
-          className={`h-screen sidebar bg-[#FAEDCD] shadow-sm shadow-right  ${responsive?'block':'hidden'} md:block   relative  dark:text-white  ${
-            hide ? " w-64 md:w-96  " : "w-20  "
+          className={`h-screen sidebar bg-[#121D45] shadow-sm shadow-right  ${responsive?'block':'hidden'} md:block   relative  text-white  ${
+            hide ? " w-64 md:w-72  " : "w-20  "
           }`}
         >
           {hide ? (
             <div className="flex px-10  items-center relative pt-10">
-              <AiFillSafetyCertificate className="text-[#121D45]  text-4xl font-medium " />
-              <p className="text-[24px] ">
+              <AiFillSafetyCertificate className="text-secondary  text-4xl font-medium " />
+              <p className="text-[24px] font-bold lg:text-3xl text-shadow">
                 {" "}
-                Medi<span className="text-[#6f1d1b]  ">Doc</span>
+                Medi<span className=" text-secondary  ">Doc</span>
               </p>
               <AiOutlineArrowLeft
-                className="bg-white md:block hidden text-[#6f1d1b]   w-8 h-8 cursor-pointer shadow-lg rounded-full absolute right-[-10px]"
+                className="bg-white md:block hidden text-secondary   w-8 h-8 cursor-pointer shadow-lg rounded-full absolute right-[-10px]"
                 onClick={() => setHide((prev) => !prev)}
               />
               <AiOutlineArrowLeft
-                className="bg-white block md:hidden text-[#6f1d1b]   w-8 h-8 cursor-pointer shadow-lg rounded-full absolute right-[-10px]"
+                className="bg-white block md:hidden text-secondary  w-8 h-8 cursor-pointer shadow-lg rounded-full absolute right-[-10px]"
                onClick={()=>dispatch(Res())}
               />
             </div>
@@ -52,7 +52,7 @@ const Sideber = () => {
               } gap-4 py-4 pt-10`}
             >
               <GiHamburgerMenu
-                className="text-[#6f1d1b]   text-3xl font-medium cursor-pointer "
+                className="text-secondary   text-3xl font-medium cursor-pointer "
                 onClick={() => setHide((prev) => !prev)}
               />
             </li>
@@ -65,8 +65,8 @@ const Sideber = () => {
                   !hide && "justify-center"
                 } gap-4 py-4`}
               >
-                <TiHome className="text-[#6f1d1b]  text-3xl font-medium " />
-                {hide && <p className="text-16px text-[#6f1d1b]  ">Home</p>}
+                <TiHome className=" text-3xl font-medium " />
+                {hide && <p className="text-16px  ">Home</p>}
               </li>
               <li
                 className={`flex  items-center ${
@@ -106,7 +106,7 @@ const Sideber = () => {
           </div>
           <div className="absolute bottom-3  w-full flex justify-center">
             {hide ? (
-              <p className="text-16px  bg-[#6f1d1b]  w-auto  rounded-md  px-5 py-2 text-white ">
+              <p className="text-16px   badge-secondary  w-auto  rounded-md  px-5 py-2 text-white ">
                 New appointment
               </p>
             ) : (
@@ -115,7 +115,7 @@ const Sideber = () => {
                   !hide && "justify-center"
                 } gap-4 py-4`}
               >
-                <div className="h-12 w-12 bg-[#e53a3d] flex items-center justify-center rounded-3xl">
+                <div className="h-12 w-12 badge-secondary  flex items-center justify-center rounded-3xl">
                   <FaPlus className=" text-3xl font-medium w-8 h-8 rounded-full  text-white " />
                 </div>
               </li>
