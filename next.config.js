6cf42e6a65938bcbc0fd0424195812bf6dcb01ec
shaharia <adeptshaharia@gmail.com>
2025-08-13 15:31:33 +0600

    shaharia

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com','i.ibb.co.com'], // add the domain here
+    domains: ['images.pexels.com', 'i.ibb.co.com'], // existing domains
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'newsus.shop', // non-www domain
+          },
+        ],
+        destination: 'https://www.newsus.shop/:path*', // redirect to www
+        permanent: true, // 301 redirect
+      },
+    ];
   },
 };
 
