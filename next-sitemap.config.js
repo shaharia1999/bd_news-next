eaca4207d39f8ef9b8f61d477522608994cd122f
shaharia <adeptshaharia@gmail.com>
2025-08-13 11:40:46 +0600

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.newsus.shop', // No trailing slash at the end
+  generateRobotsTxt: true, // Also generates robots.txt
+  sitemapSize: 5000, // Optional, split if many URLs
+};
--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com','i.ibb.co.com'], // add the domain here
   },
-  // experimental: {
-  //     dynamicID: true, // Always fresh
-  // }
 };
 
 module.exports = nextConfig;
