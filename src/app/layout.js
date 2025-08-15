b548ffaf47e8378364e9ac2b18fae46d15902b2c
shaharia <adeptshaharia@gmail.com>
2025-08-15 22:55:55 +0600

    shaha

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,28 +2,44 @@ import "./globals.css";
 import Navber from "./component/Navber";
 import Footer from "./component/Footer";
 
-// I've updated the metadata to be more descriptive based on your request.
-// This will help with Search Engine Optimization (SEO).
 export const metadata = {
   title: "NewsUS: Your Source for Top News Updates",
-  description: "Stay up-to-date with the latest and most important top news stories. We publish all of the top news, updated daily for your convenience.",
+  description:
+    "Stay up-to-date with the latest and most important top news stories. We publish all of the top news, updated daily for your convenience.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* Keeping the Google site verification and robots meta tags as they are important for SEO */}
-      <meta name="google-site-verification" content="UYTNkdzJ3B4ahGpq5dimzr4mkxf1uLefMFjq0KetDkw" />
-      <meta name="robots" content="index, follow"></meta>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LJ1L2D3PTF"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LJ1L2D3PTF');
+            `,
+          }}
+        />
+        {/* SEO Meta Tags */}
+        <meta
+          name="google-site-verification"
+          content="UYTNkdzJ3B4ahGpq5dimzr4mkxf1uLefMFjq0KetDkw"
+        />
+        <meta name="robots" content="index, follow" />
+      </head>
       <body className="relative font-publicsans">
         <div>
-          <Navber></Navber>
+          <Navber />
         </div>
-        <div className="flex ">
-          {/* <Sideber className=''></Sideber> */}
-          {children}
-        </div>
-        <Footer/>
+        <div className="flex ">{children}</div>
+        <Footer />
       </body>
     </html>
   );
