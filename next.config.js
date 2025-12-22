a70838b7b6a90ce7b33eb7cffc6d2a00fa360c75
Shaharia Ahmed <86368809+shaharia1999@users.noreply.github.com>
2025-12-22 11:22:02 +0600

    Change redirect destination to bd-news-next.vercel.app

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
             value: 'newsus.shop', // non-www domain
           },
         ],
-        destination: 'https://www.newsus.shop/:path*', // redirect to www
+        destination: 'https://bd-news-next.vercel.app/:path*', // redirect to www
         permanent: true, // 301 redirect
       },
     ];
